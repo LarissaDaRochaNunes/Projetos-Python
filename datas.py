541a0f8c0cc2416d943525e5b73940dc2ed31f88
Larissa da Rocha Nunes <larissadarochanunes47@gmail.com>
2025-08-25 12:09:00 -0300

    Create datas.py
    Programa que mostra a data e a hora atual.

---
+++ datas.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+hoje=datetime.now() #Lê a data de hoje
+ano=hoje.strftime("%y")
+mes=hoje.strftime("%m")
+dia=hoje.strftime("%d")
+hora=hoje.strftime("%H")
+min=hoje.strftime("%M")
+print("{}/{}/{}".format(dia,mes,ano))
+print("{}:{}".format(hora,min))
